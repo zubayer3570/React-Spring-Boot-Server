770e4b56a9f1b9b390849782e0c346670784fe9b
zubayer <zubayer3570@gmail.com>
2024-02-01 03:08:25 +0600

    3rd-1

--- src/main/java/website/blog/implementations/SearchImpl.java
+++ src/main/java/website/blog/implementations/SearchImpl.java
@@ -3,8 +3,10 @@ package website.blog.implementations;
 import com.mongodb.client.MongoClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 import website.blog.models.Blog;
+import website.blog.repositories.BlogRepository;
 import website.blog.repositories.SearchRepository;
 
 import java.util.ArrayList;
@@ -31,8 +33,15 @@ public class SearchImpl implements SearchRepository {
     @Autowired
     MongoConverter mongoConverter;
 
+    @Autowired
+    BlogRepository blogRepository;
+
     @Override
     public List<Blog> searchBlogs(String keyword) {
+        if(keyword.equals("all")){
+            System.out.println("hello");
+            return blogRepository.findAll();
+        }
 
         List<Blog> arrayList = new ArrayList<>();
 
@@ -40,10 +49,11 @@ public class SearchImpl implements SearchRepository {
         MongoCollection<Document> collection = database.getCollection("blogs");
 
         AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
-                new Document("index", "search_index_1")
-                        .append("text",
-                                new Document("query", keyword)
-                                        .append("path", Arrays.asList("name", "text", "author"))))));
+                        new Document("index", "search_index_1")
+                                .append("text",
+                                        new Document("query", "Blog-1")
+                                                .append("path", Arrays.asList("name", "text", "author")))),
+                new Document("$limit", 5L)));
 
         result.forEach(document -> arrayList.add(mongoConverter.read(Blog.class, document)));
         return arrayList;
