8337fa8d49e589a1a1c482970e8b7a6097b53080
zubayer <zubayer3570@gmail.com>
2024-02-05 13:39:15 +0600

    8th-2

--- src/main/java/website/blog/entities/Blog.java
+++ src/main/java/website/blog/entities/Blog.java
@@ -2,12 +2,14 @@ package website.blog.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 
 @Document(collection = "blogs")
 public class Blog {
 
-    @Id
+    @MongoId (value = FieldType.OBJECT_ID)
     private String _id;
     private String author;
     private String name;
